f006cd40acee2765b8ed14c0fd4c23546da2c79b
Yuan Gao <meseta@gmail.com>
2013-09-07 14:26:46 +0100

    sorted out eeprom.c

--- Thalamus/eeprom.h
+++ Thalamus/eeprom.h
@@ -2,63 +2,30 @@
 // *** EEPROM Functions
 // ****************************************************************************
 
+#ifndef __EEPROM_H__
+#define __EEPROM_H__
+
+#define EEPROM_MAX_PARAMS   100 // this should be greater than or equal to the above number of parameters
+#define EEPROM_OFFSET   0 // EEPROM Offset used for moving the EEPROM values around storage (wear levelling I guess)
+#define EEPROM_VERSION	10 // version of variables in EEPROM, change this value to invalidate EEPROM contents and restore defaults
+
+extern unsigned int paramCount;
+extern unsigned int paramSendCount;
+extern unsigned char paramSendSingle;
+
+typedef struct paramStorage_struct {
+	char name[16];
+	float value;
+} paramStorage_t;
+
+extern struct paramStorage_struct paramStorage[];
+
 // *** This function loads all parameters from EEPROM.  First it loads the
 // parameters into temporary storage to verify the checksum.  Only if the
 // checksums are correct will the function update the parameters in RAM.
-void eeprom_load_all(void) {
-	unsigned char chkA, chkB;
-	unsigned int i;
-	unsigned char * ptr;
-	float tempStorage[EEPROM_MAX_PARAMS+1];
-	
-	// Read EEPROM into some temporarily allocated space
-	EEPROMRead(EEPROM_OFFSET, (unsigned char *)&tempStorage, paramCount * 4 + 2);
-	
-	// Calculate the Fletcher-16 checksum
-	chkA=EEPROM_VERSION;
-	chkB=EEPROM_VERSION;
-	ptr = (unsigned char *)&tempStorage;
-	for(i=0; i<paramCount*4; i++) {
-		chkA += ptr[i];
-		chkB += chkA;
-	}
-	
-	// Verify the checksum is valid (at this point i points to the correct elements for chkA and chkB)
-	if(chkA == ptr[i] && chkB == ptr[i+1]) {
-		// For valid data, load into parameters in RAM
-		for(i=0; i<paramCount; i++) {
-			paramStorage[i].value = tempStorage[i];
-		}
-	}
-}
+void eeprom_load_all(void);
+void eeprom_save_all(void);
+void eeprom_load_all2(void);
+void eeprom_save_all2(void);
 
-// *** This function saves all parameters to EEPROM.
-void eeprom_save_all(void) {
-	unsigned char chkA, chkB;
-	unsigned int i;
-	unsigned char * ptr;
-	float tempStorage[EEPROM_MAX_PARAMS+1];
-	
-	// Save parameters into temporary space
-	
-	chkA=EEPROM_VERSION;
-	chkB=EEPROM_VERSION;
-	for(i=0; i<paramCount && i<EEPROM_MAX_PARAMS; i++) {
-		tempStorage[i] = paramStorage[i].value;
-		ptr = (unsigned char *)&(tempStorage[i]);
-		chkA += ptr[0];
-		chkB += chkA;
-		chkA += ptr[1];
-		chkB += chkA;
-		chkA += ptr[2];
-		chkB += chkA;
-		chkA += ptr[3];
-		chkB += chkA;
-	}
-	
-	ptr = (unsigned char *)&(tempStorage[i]);
-	ptr[0] = chkA;
-	ptr[1] = chkB;
-	
-	EEPROMWrite(EEPROM_OFFSET, (unsigned char *)&tempStorage, paramCount * 4 + 2);
-}
+#endif
--- Thalamus/main.c
+++ Thalamus/main.c
@@ -36,6 +36,8 @@
 #include "mavlink.h"
 #include <math.h>
 
+#include "eeprom.h"
+
 
 // ****************************************************************************
 // ****************************************************************************
@@ -69,11 +71,6 @@
 
 #define MAXTHRESH		   750 //(MAXSTICK+MIDSTICK)/2 - 50
 #define MINTHRESH		   250 //(MIDSTICK+OFFSTICK)/2 + 50
-
-#define EEPROM_MAX_PARAMS   100 // this should be greater than or equal to the above number of parameters
-#define EEPROM_OFFSET   0 // EEPROM Offset used for moving the EEPROM values around storage (wear levelling I guess)
-#define EEPROM_VERSION	10 // version of variables in EEPROM, change this value to invalidate EEPROM contents and restore defaults
-
 //  Running Average Lengths
 #define GAV_LEN 8
 #define AAV_LEN 30
@@ -210,10 +207,6 @@ typedef struct{
 } threeAxisSensorStructMag;
 threeAxisSensorStructMag Mag;
 
-typedef struct paramStorage_struct {
-	char name[16];
-	float value;
-} paramStorage_t;
 
 
 /////////////////////////////////// GLOBAL VARIABLES /////////////////////////////////
@@ -224,10 +217,6 @@ unsigned long long sysUS;
 unsigned short RxWatchdog;
 unsigned short UltraWatchdog;
 unsigned short slowSoftscale;
-
-unsigned int paramSendCount;
-unsigned int paramCount;
-unsigned char paramSendSingle;
 
 // LEDs
 unsigned char flashPLED, flashVLED, flashRLED;
@@ -305,189 +294,6 @@ float YAW_SPL_set;
 
 /////////////////////////////////////////// TUNABLE PARAMETERS ////////////////////////////////////
 
-struct paramStorage_struct paramStorage[] = {
-	{"DRIFT_AKp",		   0.2f},
-	{"DRIFT_MKp",	  		0.2f},   
-	#define DRIFT_AccelKp   paramStorage[0].value	
-	#define DRIFT_MagKp	 paramStorage[1].value  
-  
-	{"LPF_ULTRA",	   0.05f},  
-	#define LPF_ULTRA 		paramStorage[2].value
-
-	{"YAW_SEN",	 0.0001f},	 
-	{"PITCH_SEN",	0.0022f},	
-	{"ROLL_SEN",	 0.0022f},  
-	{"YAW_DZN",	  0.001f},	
-	#define YAW_SENS		paramStorage[3].value
-	#define PITCH_SENS	  paramStorage[4].value
-	#define ROLL_SENS	   paramStorage[5].value
-	#define YAW_DEADZONE	paramStorage[6].value 
-	
-	{"PITCH_Kp",	  400.0f},	 
-	{"PITCH_Ki",		2.0f},	
-	{"PITCH_Kd",	  100.0f},	 
-	{"PITCH_Kdd",	1500.0f},
-	{"PITCH_Bst",	 0.0f},	  
-	{"PITCH_De",	  0.999f},	
-	#define PITCH_Kp		paramStorage[7].value 
-	#define PITCH_Ki		paramStorage[8].value
-	#define PITCH_Kd		paramStorage[9].value 
-	#define PITCH_Kdd	   paramStorage[10].value 
-	#define PITCH_Boost	 paramStorage[11].value 
-	#define PITCH_De		paramStorage[12].value 
-
-	{"ROLL_Kp",	   400.0f},	   
-	{"ROLL_Ki",		 2.0f},	  
-	{"ROLL_Kd",	   100.0f},		
-	{"ROLL_Kdd",	 1500.0f},	 		
-	{"ROLL_Bst",	   0.00f},					
-	{"ROLL_De",	   0.999f},
-	#define ROLL_Kp		 paramStorage[13].value  
-	#define ROLL_Ki		 paramStorage[14].value 
-	#define ROLL_Kd		 paramStorage[15].value 
-	#define ROLL_Kdd		paramStorage[16].value 	
-	#define ROLL_Boost	  paramStorage[17].value 
-	#define ROLL_De		 paramStorage[18].value
-
-	{"YAW_Kp",		1000.0f},	 
-	{"YAW_Kd",		250.0f},  
-	{"YAW_Bst",		0.00f},   
-	#define YAW_Kp		  paramStorage[19].value 
-	#define YAW_Kd		  paramStorage[20].value 
-	#define YAW_Boost	   paramStorage[21].value 	
-
-	// Mode
-	{"state",	   STATE_DISARMED},  //
-	#define state 		paramStorage[22].value
-
-	//Limits
-	{"LIM_ANGLE",	   0.35f},  // Roll and Pitch Angle Limit in Radians
-	{"LIM_ALT",		 1000.0f},  // Altitude Limit in mm when in Ultrasound Mode
-	#define LIM_ANGLE 		paramStorage[23].value
-	#define LIM_ALT 		paramStorage[24].value
-
-	// Magneto Correction
-	{"CAL_MAGN1",	 0.001756f},	  
-	{"CAL_MAGN2",   0.00008370f},		
-	{"CAL_MAGN3",   0.00005155f},	 	
-	{"CAL_MAGN5",	 0.001964f},			
-	{"CAL_MAGN6",   0.00002218f},	 
-	{"CAL_MAGN9",	 0.001768f},	 
-	{"CAL_MAGM1",	   0.0f},	 
-	{"CAL_MAGM2",		0.0f},	 	
-	{"CAL_MAGM3",		0.0f},  
-	#define MAGCOR_N1	   paramStorage[25].value 
-	#define MAGCOR_N2	   paramStorage[26].value 		
-	#define MAGCOR_N3	   paramStorage[27].value 		
-	#define MAGCOR_N5	   paramStorage[28].value 
-	#define MAGCOR_N6	   paramStorage[29].value 
-	#define MAGCOR_N9	   paramStorage[30].value 
-	#define MAGCOR_M1	   paramStorage[31].value 		
-	#define MAGCOR_M2	   paramStorage[32].value 
-	#define MAGCOR_M3	   paramStorage[33].value
-
-	// Ultrasound
-	{"ULTRA_Kp",		0.05f},
-	{"ULTRA_Kd",		0.1f},
-	{"ULTRA_Ki",		0.00001f},
-	{"ULTRA_De",	  	0.9999f},
-	{"ULTRA_TKOFF",   	200.0f}, 
-	{"ULTRA_LND",   	150.0f}, 
-	#define ULTRA_Kp		paramStorage[34].value 
-	#define ULTRA_Kd		paramStorage[35].value 
-	#define ULTRA_Ki		paramStorage[36].value 
-	#define ULTRA_De		paramStorage[37].value
-	#define ULTRA_TKOFF	 paramStorage[38].value 
-	#define ULTRA_LND	   paramStorage[39].value
-
-	// TODO: I don't think these should be tunable parameters should they? Remember that the gyros are calibrated on every Arm
-	{"CAL_GYROX",   0.0f},
-	{"CAL_GYROY",   0.0f},
-	{"CAL_GYROZ",   0.0f},
-	#define CAL_GYROX	   paramStorage[40].value 
-	#define CAL_GYROY	   paramStorage[41].value 
-	#define CAL_GYROZ	   paramStorage[42].value 
-	
-	{"DETUNE",			0.2f},
-	#define DETUNE		paramStorage[43].value
-	
-	{"LIM_RATE",			100.0f},
-	#define LIM_RATE		paramStorage[44].value   
-	{"LIM_ULTRA",			4.0f},
-	#define LIM_ULTRA		paramStorage[45].value
-	
-	{"ULTRA_DRMP",	 3.0f}, 
-	{"ULTRA_DTCT",	 6.0f},
-	#define ULTRA_DRMP	  paramStorage[46].value
-	#define ULTRA_DTCT	  paramStorage[47].value
-	
-	{"LIM_THROT", 		0.3f},
-	#define LIM_THROT		paramStorage[48].value
-	
-	{"ULTRA_OVDEC",		0.01f},
-	#define ULTRA_OVDEC		paramStorage[49].value
-	
-	{"ULTRA_DEAD",		100},
-	#define ULTRA_DEAD		paramStorage[50].value
-	
-	{"ULTRA_OVTH",		40},
-	#define ULTRA_OVTH		paramStorage[51].value
-  
-	
-	{"CAL_AUTO", 1.0f},
-	#define CAL_AUTO		paramStorage[52].value	
-	
-	
-	{"LPF_OUT",	   0.6f},  
-	#define LPF_OUT 		paramStorage[53].value
-	
-	
-	{"BAT_LOW",		 11000.0f},
-	{"BAT_CRIT",		10000.0f},
-	#define BATT_LOW		paramStorage[54].value
-	#define BATT_CRIT	   paramStorage[55].value
-	
-	{"ULTRA_OFFSET",		 350},
-	#define ULTRA_OFFSET		paramStorage[56].value
-	
-	{"ROLL_SPL",		 0.02},
-	#define ROLL_SPL		paramStorage[57].value
-	{"PITCH_SPL",		 0.02},
-	#define PITCH_SPL		paramStorage[58].value
-	
-	// TODO: Tune Yaw integral
-	{"YAW_Ki",		 0.0},
-	#define YAW_Ki		paramStorage[59].value
-	{"YAW_De",		 1.0},
-	#define YAW_De		paramStorage[60].value
-
-	{"Filt_GPS_K",		 1.0},
-	#define Filt_GPS_K		paramStorage[61].value
-
-	{"LPF_BARO",   0.05},
-	 #define LPF_BARO  paramStorage[62].value
-
-	{"GPS_ALTKp", 5.0f},
-    {"GPS_ALTKi", 0.0001f},
-    {"GPS_ALTDe", 1.0f},
-    {"GPS_ALTKd", 20.0f},
-    #define GPS_ALTKp paramStorage[63].value
-    #define GPS_ALTKi paramStorage[64].value
-    #define GPS_ALTDe paramStorage[65].value
-    #define GPS_ALTKd paramStorage[66].value
-
-    {"Filt_baroK",		 0.0},
-	#define Filt_baroK		paramStorage[67].value
-	
-	{"YAW_SPL",		 0.04},
-	#define YAW_SPL		paramStorage[68].value
-    
-	{"ORI",		 0.00},
-	#define ORI		paramStorage[69].value
-	
-	};
-
-
 
 #include "setup.h"
 #include "filter.h"
@@ -495,7 +301,6 @@ struct paramStorage_struct paramStorage[] = {
 #include "states.h"
 #include "control.h"
 #include "inputs.h"
-#include "eeprom.h"
 #include "communication.h"
 
 
